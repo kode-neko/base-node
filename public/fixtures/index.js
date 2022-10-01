4e3310c6161143657f92a0438670d838a9135585
Kodeneko <ladysun.freedom@gmail.com>
2022-10-01 17:18:00 +0200

    [base-node-mongo] Refactor fir & fix config

--- public/fixtures/index.js
+++ public/fixtures/index.js
@@ -3,6 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.exampleRouer = void 0;
-var example_1 = require("./example");
-Object.defineProperty(exports, "exampleRouer", { enumerable: true, get: function () { return __importDefault(example_1).default; } });
+exports.exampleFix = void 0;
+var example_json_1 = require("./example.json");
+Object.defineProperty(exports, "exampleFix", { enumerable: true, get: function () { return __importDefault(example_json_1).default; } });
--- public/src/utils/mongo/config/index.js
+++ public/src/utils/mongo/config/index.js
@@ -1,13 +1,10 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.mongoConfigExample = exports.mongoConfig = void 0;
-const example_json_1 = __importDefault(require("../../../../fixtures/example.json"));
+const _fixtures_1 = require("@fixtures");
 const mongoConfigExample = {
     name: 'example',
-    fixtures: example_json_1.default,
+    fixtures: _fixtures_1.exampleFix,
 };
 exports.mongoConfigExample = mongoConfigExample;
 const mongoConfig = [mongoConfigExample];
