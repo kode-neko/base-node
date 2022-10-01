d141d06e79f83ea910b8803e147ad8a57c77fc39
Kodeneko <ladysun.freedom@gmail.com>
2022-10-01 17:30:47 +0200

    [dev] included bootstrap

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,11 @@ import {
 } from './routes';
 
 const app = express();
-console.log('PATATA', __dirname);
-app.use('/assets/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')));
-app.use('/assets/js', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/js')));
+app.use('/assets/css', express.static(path.join(__dirname, '../../node_modules/bootstrap/dist/css')));
+app.use('/assets/js', express.static(path.join(__dirname, '../../node_modules/bootstrap/dist/js')));
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
-app.set('views', path.resolve('../templates'));
+app.set('views', path.resolve('./templates'));
 app.use('/example', exampleRouer);
 app.use(express.json());
 
